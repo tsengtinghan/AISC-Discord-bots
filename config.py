d5bb7599ce69d54b1d6811f25181cc7d8ace365f
Michael Tseng <78808751+tsengtinghan@users.noreply.github.com>
2023-03-20 18:36:28 +0800

    Delete config.py
    delete tokens

--- config.py
+++
@@ -1,7 +0,0 @@
-TOKEN = 'MTA4Njg1MTg5MjU5ODAzMDQxOQ.Gpbrdf.i6CiPOuizTiAiulPwpJrYHLXps5fSiig9eaAII'
-
-GUILD_ID = 1086095100792279070  # Replace with your guild ID
-INPUT_CHANNEL_ID = 1086905931901718539  # Replace with your input channel ID
-OUTPUT_CHANNEL_ID = 1086905964801822741  # Replace with your output channel ID
-
-OPENAI_API_KEY = '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'
